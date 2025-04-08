36a7446b38204604bd2619b6bf62b08e9d5a3b64
C.C <fanweixiao@gmail.com>
2025-04-08 15:59:16 +0800

    fix fetch with proxy

--- node-tool-get-weather-google-api/app.ts
+++ node-tool-get-weather-google-api/app.ts
@@ -1,5 +1,7 @@
+import fetch, { RequestInit } from 'node-fetch';
+import { SocksProxyAgent } from 'socks-proxy-agent';
 
-// check if the environment variable is set
+// env GOOGLE_CLOUD_API_KEY must be set
 if (!process.env.GOOGLE_CLOUD_API_KEY) {
   throw new Error("GOOGLE_CLOUD_API_KEY is not set");
 }
@@ -18,11 +20,11 @@ export type Argument = {
 
 export async function handler(args: Argument) {
   try {
-    console.log(`>Getting weather info for address [${args.address}]`);
+    console.log(`> trigger: [${args.address}]`);
     const geo = await getGeocode(args.address);
     console.log(`  [address=${args.address}] Geocode data:`, geo);
     const weather = await getWeather(geo.lat, geo.lng);
-    console.log(`  [address=${args.address}] Weather data:`, weather);
+    console.log(`  [address=${args.address}] Weather data:`, weather.timeZone);
     return {
       ok: true,
       result: weather,
@@ -36,20 +38,56 @@ export async function handler(args: Argument) {
   }
 }
 
+type GeocodeResponse = {
+  results: Array<{
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      }
+    }
+  }>
+};
+
+type WeatherResponse = {
+  currentTime: string;
+  timeZone: {
+    id: string;
+  };
+  weatherCondition: {
+    iconBaseUri: string;
+    description: {
+      text: string;
+      languageCode: string;
+    };
+    type: string;
+  };
+  temperature: {
+    degrees: number;
+    unit: string;
+  };
+  feelsLikeTemperature: {
+    degrees: number;
+    unit: string;
+  };
+  windChill: {
+    degrees: number;
+    unit: string;
+  };
+  relativeHumidity: number;
+  thunderstormProbability?: number;
+  cloudCover?: number;
+}
+
+
+// get the lat and lng of the address by google geocoding api
 export async function getGeocode(address: string) {
-  // get the lat and lng of the address by google geocoding api
-  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_CLOUD_API_KEY}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      signal: AbortSignal.timeout(5000), // 5000ms timeout
-    }).then(async response => {
+  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_CLOUD_API_KEY}`, fetchOptions()).then(async response => {
       if (!response.ok) {
         throw new Error(`Geocode API error: ${response.status} ${response.statusText}`);
       }
       
-      const geocode = await response.json();
+      const geocode = await response.json() as GeocodeResponse;
       
       if (geocode.results.length === 0) {
         throw new Error(`No results found for address [${address}]`);
@@ -65,24 +103,53 @@ export async function getGeocode(address: string) {
     });
 }
 
+// get the weather info by google weather api
 export async function getWeather(lat: number, lng: number) {
   const url = `https://weather.googleapis.com/v1/currentConditions:lookup?key=${GOOGLE_CLOUD_API_KEY}&location.latitude=${lat}&location.longitude=${lng}`;
 
-  return fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    signal: AbortSignal.timeout(5000), // 5000ms timeout
-  })
-    .then(response => {
+  return fetch(url, fetchOptions())
+    .then(async response => {
       if (!response.ok) {
         throw new Error(`Weather API error: ${response.status} ${response.statusText}`);
       }
-      return response.json();
+      const data = await response.json();
+      return data as WeatherResponse;
     })
     .catch(error => {
       console.error(`Error fetching weather data for [lat=${lat}, lng=${lng}]:`, error);
       throw error;
     });
+}
+
+// Extended RequestInit interface to include agent property
+interface ExtendedRequestInit extends RequestInit {
+  agent?: SocksProxyAgent;
+}
+
+// introduce socks5 proxy if is present for dev/test env
+let socksProxyAgent : SocksProxyAgent | undefined = undefined;
+if (process.env.NODE_ENV !== 'production') {
+  if (process.env.ALL_PROXY || process.env.all_proxy) {
+    console.log(`** Using proxy: ${process.env.ALL_PROXY || process.env.all_proxy}`);
+    socksProxyAgent = new SocksProxyAgent(process.env.ALL_PROXY || process.env.all_proxy as string);
+  } else {
+    console.log(`** No proxy being used - 'ALL_PROXY' or 'all_proxy' environment variables not found`);
+  }
+}
+
+// Function to fetch options with proxy and timeout support
+function fetchOptions(): ExtendedRequestInit {
+  const options: ExtendedRequestInit = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    signal: AbortSignal.timeout(3000),
+  };
+
+  if (socksProxyAgent) {
+    options.agent = socksProxyAgent;
+  }
+
+  return options;
 }
